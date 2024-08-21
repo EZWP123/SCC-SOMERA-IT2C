fe46c8d0e7a2b5928567078dd4ead6cf33ce4744
clarence <clarenceparan1@gmail.com>
2024-08-21 14:53:38 +0800

    file for product

---
+++ src/javaappsecc/Grades.java
@@ -0,0 +1,23 @@
+
+package javaappsecc;
+
+public class Grades {
+    
+int id;
+String name;
+double p, m, pf, f;
+
+
+public  void addGrades(int sid, String name, double prl, double mid, double prf, double fnl);
+this.name = name;
+this.id = sid;
+
+
+    
+    
+    
+    
+    
+    
+    
+}
--- src/javaappsecc/JavaAppSecC.java
+++ src/javaappsecc/JavaAppSecC.java
@@ -7,10 +7,16 @@ import java.util.Scanner;
 
 public class JavaAppSecC {
     public static void main(String[] args) {
-        
-    Payget pg = new Payget();
-    pg.getPay();
-        
+//        
+//    Payget pg = new Payget();
+//    pg.getPay();
+//        
+//    }
+//    
+//}
+
+Product pr = new Product();
+pr.addProduct(1011, "soap",20,30,20.00);
+pr.viewProduct();
     }
     
-}
