9f2e1a5940714e0a540b8317f20431314705d18f
clarenceparan <clarenceparan1@gmail.com>
2024-08-28 15:19:24 +0800

    Added students class

--- src/javaappsecc/Grades.java
+++ src/javaappsecc/Grades.java
@@ -1,23 +1,40 @@
 
 package javaappsecc;
 
+
+
 public class Grades {
     
-int id;
-String name;
-double p, m, pf, f;
+   int id;
+   String name;
+   double p, m, pf, f;
+   double average;
+   public void addGrades(int sid, String name, double prelim, double midterm, double prefinal, double finals){
+  
+    
+        this.id = sid;
+        this.name = name;
+        this.pf = prefinal;
+        this.m = midterm;
+        this.p = prelim;
+        this.f = finals;
+       
+    }
 
+     public void viewGrades() {
+         double average = (this.p + this.m + this.pf + this.f)/4;
+         String remarks = (average > 3.0) ? "Failed" : "Passed";
+        System.out.printf("%-10d %-10s %-10.2f %-10.2f %-10.2f %-10.2f %-10.2f %-10s\n",
+                          this.id, this.name, this.p, this.m, this.pf, this.f, average, remarks);
 
-public  void addGrades(int sid, String name, double prl, double mid, double prf, double fnl);
-this.name = name;
-this.id = sid;
-
-
-    
-    
-    
-    
-    
-    
-    
+        
+    }
+     
 }
+
+
+
+
+
+
+
--- src/javaappsecc/students.java
+++ src/javaappsecc/students.java
@@ -1,42 +1,13 @@
-
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package javaappsecc;
-
 import java.util.Scanner;
 
-
-public class JavaAppSecC {
-    public static void main(String[] args) {
-//        
-//    Payget pg = new Payget();
-//    pg.getPay();
-//        
-//    }
-//    
-//}
-
-//        Product pr = new Product();
-//        pr.addProduct(1011, "soap",20,30,20.00);
-//        pr.viewProduct();
-//            }
-//Grades gr = new Grades();
-//Scanner sc = new Scanner (System.in);
-//
-//
-//    System.out.printf("%-10s %-10s %-10s %-10s %-10s %-10s %-10s %-10s\n",
-//                      "Student ID", "Name", "Prelim", "Midterm", "Prefinal", "Final", "Average", "Remarks"); 
-//
-//gr.addGrades(1001, "Mike",1.0,1.0, 1.0, 1.0);
-//gr.viewGrades();
-//
-//Grades gr1 = new Grades ();
-//gr1.addGrades(1002, "john", 2.2, 1.0, 1.0, 1.0);
-//gr1.viewGrades();
-//
-//}
-//}
-
-
+public class students {
+    public void genGrade() {
         Scanner sc = new Scanner(System.in);
         Grades[] gr = new Grades[100];
 
@@ -73,3 +44,6 @@ public class JavaAppSecC {
         }
     }
 }
+
+   
+
